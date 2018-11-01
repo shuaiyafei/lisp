36cfee667e7f5dd32b3ecdab64f8a7eefb96f22a
woaigmz <woaigmz@gmail.com>
2018-11-01 18:36:09 +0800

    mine page first

---
+++ src/localData/MinePageLocalData.js
@@ -0,0 +1,40 @@
+exports.header = {
+  username: "李白",
+  avatar: "https://avatars3.githubusercontent.com/u/16618929?s=88&v=4"
+};
+
+exports.body = [
+
+  {
+    icon: "",
+    des: "订单"
+  },
+  {
+    icon: "",
+    des: "历史记录"
+  },
+  {
+    icon: "",
+    des: "推荐"
+  }
+
+];
+
+
+exports.ad = {
+  url: ""
+}
+
+exports.list = [
+
+  {
+    title: "常见问题"
+  },
+  {
+    title: "平台规则"
+  },
+  {
+    title: "联系客服"
+  }
+
+];
